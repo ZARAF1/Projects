07eb0240d2c6f6262a4103eb918bc634b44308a1
Zulfiqar Ali Raffad <ali.raffad@gmail.com>
2021-05-29 02:55:45 +0300

    Update script.js
    Added Alternate abs method in end of document which includes my own abs method huraaaaayyyy!!!

--- Javascript/1 to 4 Main Project/number-guesser-starting/script.js
+++ Javascript/1 to 4 Main Project/number-guesser-starting/script.js
@@ -34,3 +34,56 @@ let advanceRound=()=>{
     currentRoundNumber++;
 }
 
+/*
+
+// Alternate solution with my own abs method 
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () =>{
+    const randomNumber = Math.floor(Math.random()* 10)
+    return randomNumber;
+}
+let randomNumber=generateTarget();
+
+const absVal = guess =>{
+    if (guess<0){
+        guess=guess*-1;
+        return guess;
+    }
+}
+
+const compareGuesses = (humanGuess,computerGuess,randomNumber)=>{
+    let humanCloseness=humanGuess-randomNumber;
+    let computerCloseness = computerGuess-randomNumber;
+    let humanFinal = absVal(humanCloseness);
+    let computerFinal = absVal(computerCloseness);
+    if (humanFinal<computerFinal){
+        return true;
+    }
+    else if (humanFinal>computerFinal){
+        return false;
+    }
+    else if(humanFinal===computerFinal){
+        return true;
+    }
+}
+
+const updateScore = (winner)=>{
+    let humanScore;
+    let computerScore;
+    if(winner==='human'){
+        humanScore= humanScore+1;
+    }
+    else if (winner==='computer'){
+        computerScore=computerScore+1;
+    }
+}
+
+const advanceRound=()=>{
+    currentRoundNumber = currentRoundNumber + 1;
+
+}
+*/
