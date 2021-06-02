ddf4fd3493e78e656917497e075a1f20c4951a52
Zulfiqar Ali Raffad <ali.raffad@gmail.com>
2021-06-02 04:23:33 +0300

    Create convertToBaby.js

---
+++ Javascript/9. Lodash Library/convertToBaby.js
@@ -0,0 +1,28 @@
+/*
+convertToBaby()
+Instructions
+1.
+Write a function, convertToBaby(), that takes in an array as an argument and, using a loop, returns a new array with
+each string in the array prepended with 'baby '.
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+convertToBaby(animals);
+// Should return ['baby panda', 'baby turtle', 'baby giraffe', 'baby hippo', 'baby sloth', 'baby human'];
+Note: You may have noticed how convenient it would be to use map, but you’re not allowed to use it here. Don’t worry
+you’ll have plenty of opportunities to use built-in methods later on!*/
+
+// Write your code here:
+convertToBaby =(arr)=>{
+    let newArr=[];
+    for (let i=0;i<arr.length;i++){
+        newArr.push(`baby ${arr[i]}`)
+    }
+    return newArr;
+}
+
+
+
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+
+console.log(convertToBaby(animals))
